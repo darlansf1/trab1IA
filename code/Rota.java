a4489acfe12ed0907d1f4724b9def1b7197ff131
Darlan Santana Farias <darlansf1@gmail.com>
2015-09-22 23:32:54 -0300

    Implementada busca em largura no metodo achar_rota

--- code/Rota.java
+++ code/Rota.java
@@ -1,7 +1,6 @@
-import java.util.Scanner;
+import java.util.*;
 import java.io.*;
 import java.awt.Point;
-import java.util.ArrayList;
 
 
 public class Rota{
@@ -23,7 +22,82 @@ public class Rota{
 
 
 
-	public  ArrayList<Point> achar_rota(){ return null; }
+	public  ArrayList<Point> achar_rota(int[] destino){
+            ArrayList<Point> fila = new ArrayList<Point>();
+            int visitados[][] = new int[lin][col];
+            HashMap<Point, Point> antecessores = new HashMap<Point, Point>();
+            
+            fila.add(new Point(ini_p[0], ini_p[1]));
+            Point p = fila.get(0);
+            antecessores.put(p, null);
+            visitados[(int)p.getX()][(int)p.getY()] = 1;
+            
+            while(p.getX() != destino[0] || p.getY()!= destino[1] || fila.isEmpty()){
+                //remove pai da fila
+                fila.remove(p);
+                
+                //adiciona filhos
+                int x = (int) p.getX();
+                int y = (int) p.getY();
+                Point filho1 = null; 
+                Point filho2 = null; 
+                Point filho3 = null; 
+                Point filho4 = null;
+                
+                //System.out.println("p: "+x+", "+y);
+                if(p.getX() > 0 && mapa[x-1][y] == 0 && visitados[x-1][y] == 0){
+                    filho1 = new Point(x-1, y);
+                    visitados[x-1][y] = 1;
+                    fila.add(filho1);
+                    //System.out.println("f1: "+(x-1)+", "+y);
+                }if(p.getY() > 0 && mapa[x][y-1] == 0 && visitados[x][y-1] == 0){
+                    filho2 = new Point(x, y-1);
+                    visitados[x][y-1] = 1;
+                    fila.add(filho2);
+                    //System.out.println("f2: "+x+", "+(y-1));
+                }if(p.getX()+1 < lin && mapa[x+1][y] == 0 && visitados[x+1][y] == 0){
+                    filho3 = new Point(x+1, y);
+                    visitados[x+1][y] = 1;
+                    fila.add(filho3);
+                    //System.out.println("f3: "+(x+1)+", "+y);
+                }if(p.getY()+1 < col && mapa[x][y+1] == 0 && visitados[x][y+1] == 0){
+                    filho4 = new Point(x, y+1);
+                    visitados[x][y+1] = 1;
+                    fila.add(filho4);
+                    //System.out.println("f4: "+x+", "+(y+1));
+                }
+               
+               //new Scanner(System.in).nextLine();
+                
+                //adiciona pai a lista de sucessores dos filhos
+                if(filho1 != null)
+                    antecessores.put(filho1, p);
+                if(filho2 != null)
+                    antecessores.put(filho2, p);
+                if(filho3 != null)
+                    antecessores.put(filho3, p);
+                if(filho4 != null)
+                    antecessores.put(filho4, p);
+                
+                if(!fila.isEmpty())
+                    p = fila.get(0);
+            }
+            
+            ArrayList<Point> caminho = new ArrayList<Point>();
+            caminho.add(p);
+            while(antecessores.get(p) != null){
+                p = antecessores.get(p);
+                caminho.add(p);
+            }
+            //System.out.println("Caminho encontrado:");
+            ArrayList<Point> caminho_final = new ArrayList<Point>();
+            for(int i = caminho.size()-1; i >= 0; i--){
+                caminho_final.add(caminho.get(i));
+                //System.out.println("linha: "+caminho.get(i).getX()+" coluna: "+caminho.get(i).getY());
+            }
+            
+            return caminho_final;
+        }
 	public  ArrayList<Point> achar_rota_h(){ return null; }
 
 
@@ -36,10 +110,10 @@ public class Rota{
 
 			this.lin = lerArq.nextInt();
 			this.col = lerArq.nextInt();
-			this.pessoa_p[0] =  lerArq.nextInt();
-			this.pessoa_p[1] = lerArq.nextInt(); 
-			this.ini_p[1] = lerArq.nextInt();
-			this.ini_p[1] = lerArq.nextInt();
+			this.ini_p[0] =  lerArq.nextInt();
+			this.ini_p[1] = lerArq.nextInt(); 
+			this.pessoa_p[0] = lerArq.nextInt();
+			this.pessoa_p[1] = lerArq.nextInt();
 
 			this.mapa = new int[lin][col];
 
@@ -63,6 +137,7 @@ public class Rota{
 			System.out.println();
 		}
 
+                achar_rota(pessoa_p);
 	}
 
 }
